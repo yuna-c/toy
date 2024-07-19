22bb9c9b55ac0bc3c91222b3a679c207d26ad965
yuna <yuna744@gmail.com>
2024-07-20 04:15:37 +0900

    ✨ update : 추억 앨범 만들기

---
+++ Album/firebase.js
@@ -0,0 +1,26 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  
+  const firebaseConfig = {
+    apiKey: "AIzaSyCNuRi81cTJLYcOMvVKlO65f4zSX_fJ9oQ",
+    authDomain: "sparta-5a01b.firebaseapp.com",
+    projectId: "sparta-5a01b",
+    storageBucket: "sparta-5a01b.appspot.com",
+    messagingSenderId: "1027354777949",
+    appId: "1:1027354777949:web:f41dfd533ecf465200f0b4",
+    measurementId: "G-395DV8PFN5"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
+</script>
+
